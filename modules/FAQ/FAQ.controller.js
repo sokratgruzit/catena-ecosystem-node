676e3f6f8727f98721e7ed72f7fd871b98285b67
child <pipiaalex7@gmail.com>
2023-05-12 18:54:58 +0400

    add routes

--- modules/FAQ/FAQ.controller.js
+++ modules/FAQ/FAQ.controller.js
@@ -4,12 +4,11 @@ import { FAQ } from "../../models/FAQ.js";
 export const create = async (req, res) => {
     try {
         const { question, answer } = req.body;
-        console.log(question, answer)
+
         let result = new FAQ({
             question,
             answer,
         });
-
         result = await result.save();
         res.status(200).json({ message: "New FAQ Created", result });
     } catch (e) {
@@ -23,6 +22,48 @@ export const findOneFAQ = async (req, res) => {
         let result = await FAQ.find({
          question: req.body.question 
         });
+    
+        res.status(200).json(result);
+    } catch (e) {
+        console.log(e.message);
+        res.status(400).json({ message: e.message });
+    }
+};
+
+export const findAllFAQ = async (req, res) => {
+    try {
+        let result = await FAQ.find({ });
+    
+        res.status(200).json(result);
+    } catch (e) {
+        console.log(e.message);
+        res.status(400).json({ message: e.message });
+    }
+};
+
+export const updateOneFAQ = async (req, res) => {
+    try {
+        const { _id } = req.body;
+ 
+        const result = await FAQ.findOneAndUpdate({ _id }, req.body, {
+            new: true,
+          });
+    
+        res.status(200).json(result);
+    } catch (e) {
+        console.log(e.message);
+        res.status(400).json({ message: e.message });
+    }
+};
+
+export const changeStatus = async (req, res) => {
+    try {
+        const { _id } = req.body;
+ 
+        const result = await FAQ.findOneAndUpdate({ _id }, req.body, {
+            new: true,
+          });
+
         console.log(req.body)
     
         res.status(200).json(result);
@@ -34,12 +75,12 @@ export const findOneFAQ = async (req, res) => {
 
 export const destroyOneFAQ = async (req, res) => {
     try {
-        const result = await FAQ.deleteOne({ question: req.body.question });
+        const result = await FAQ.deleteOne({ _id: req.body._id});
 
         if (result.acknowledged === true) {
           return res.status(200).json({ message: "FAQ successuly deleted" });
         }
-        res.status(400).json({ message: "Article deletion failed" });
+        res.status(400).json({ message: "FAQ deletion failed" });
       } catch (e) {
         console.log(e.message);
         res.status(400).json({ message: e.message });
