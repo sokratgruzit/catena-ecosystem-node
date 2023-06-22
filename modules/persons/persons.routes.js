53e917befdd03a66df59af7ef18ea93b5ffb7acc
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-06-22 17:09:03 +0400

    feature: create person update function route

--- modules/persons/persons.routes.js
+++ modules/persons/persons.routes.js
@@ -8,6 +8,7 @@ const router = Router();
 router.route("/get-persons-image").get();
 router.route("/get-all-persons").get(personsController.getAllPersons);
 router.route("/create").post(upload.single("image"), personsController.persons);
+router.route("/update-persons").delete(personsController.updatePerson);
 router.route("/delete-persons").delete(personsController.deletePersons);
 
 export default router;
