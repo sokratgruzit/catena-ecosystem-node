ffc83b1b8138a706cc7d58a8e4fa95a13f309cbf
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-06-23 15:09:36 +0400

    feature: update person with image

--- modules/persons/persons.routes.js
+++ modules/persons/persons.routes.js
@@ -9,7 +9,7 @@ router.route("/get-persons-image").get();
 router.route("/get-all-persons").get(personsController.getAllPersons);
 // router.route("/update").put(upload.single("image"), personsController.update);
 router.route("/create").post(upload.single("image"), personsController.persons);
-router.route("/update-persons").put(personsController.updatePerson);
+router.route("/update-persons").post(upload.single("image"), personsController.updatePerson);
 router.route("/delete-persons").post(personsController.deletePersons);
 
 export default router;
