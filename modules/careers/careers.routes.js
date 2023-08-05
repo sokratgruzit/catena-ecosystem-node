f1054d38aadb582ed71b1b7a3710e36af083f004
alex <pipiaalex7@gmail.com>
2023-08-05 15:30:57 +0400

    rame

---
+++ modules/careers/careers.routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import * as careersController from "./careers.controller.js";
+
+const router = Router();
+
+router.route("/create").post(careersController.create);
+// router.route("/delete").put(careersController.deleteCareer);
+// router.route("/edit").put(careersController.editCareer)
+// router.route("/get-all-careers").get(careersController.getAllCareers);
+
+
+export default router;
