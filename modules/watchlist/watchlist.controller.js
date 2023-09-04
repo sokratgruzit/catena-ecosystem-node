b36753b1f7a2ff6275d0b3c75250da14fca37433
alex <pipiaalex7@gmail.com>
2023-09-04 19:40:15 +0400

    watchlist

---
+++ modules/watchlist/watchlist.controller.js
@@ -0,0 +1,43 @@
+import { Watchlist } from "../../models/Watchlist.js";
+import { paginateResults } from "../../utils/pagination.js";
+
+export const addOrUpdateWatchlist = async (req, res) => {
+  const {
+    user_id,
+    token_id
+  } = req.body;
+
+  try {
+    const userWatchlist = await Watchlist.findOne({ user_id });
+    if (userWatchlist) {
+      const { token_ids } = userWatchlist;
+      let newTokenIds = token_ids.push(token_id);
+
+      const updateWatchlist = await Watchlist.findOneAndUpdate({ user_id }, { token_ids: newTokenIds }, { new: true });
+
+      if (!updateWatchlist) {
+        return res.status(404).json({ error: "Career not found" });
+      } else {
+        res.status(200).json(updateWatchlist);
+      }
+    } else {
+      const createWatchlist = await Watchlist.create({ user_id, token_id });
+      res.status(200).json(createWatchlist);
+    }
+  } catch (error) {
+    console.error("Error updating Career:", error);
+    res.status(500).json({ error: "Failed to update Career" });
+  }
+};
+
+export const getAllList = async (req, res) => {
+  const { user_id } = req.body;
+
+  try {
+    const userWatchlist = await Watchlist.findOne({ user_id });
+
+    return res.status(200).json(userWatchlist);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
---
+++ modules/watchlist/watchlist.routes.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import * as careersController from "./careers.controller.js";
+
+const router = Router();
+
+router.route("/create").post(careersController.create);
+router.route("/get-all-careers").get(careersController.getAllCareers);
+router.route("/get-active-careers").get(careersController.getActiveCareers);
+router.route("/get-careers-by-id").get(careersController.getCareerById);
+router.route("/delete").put(careersController.deleteCareer);
+router.route("/update").post(careersController.updateCareer);
+router.route("/get-one-career").post(careersController.getOneCareer);
+router.route("/get-all-careers-slug").get(careersController.getAllCareerSlug);
+
+export default router; 
